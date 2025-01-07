e1fbe9411dbf21a9db123c99201a9a9a033d004c
Mendenzon Tatis Rosario <128002783+Tatis-718@users.noreply.github.com>
2025-01-07 17:11:42 +0000

    halfway mark

--- _02_04/Ticket.java
+++ _02_04/Ticket.java
@@ -2,18 +2,36 @@ package _02_04;
 
 public class Ticket {
 
-  public Ticket() {
-
-  }
+  public Ticket() {}
 
   private String destination;
   private double price;
   private boolean isReturn;
 
   // Add three methods to set the value of each field, called setDestination,
+  public String setDestination(String dest) {
+    return destination = dest;
+  };
+
   // setPrice and setIsReturn.
+  public double setPrice(double cost){
+    return price = cost;
+  };
+  public boolean setIsReturn(boolean bool){
+    return isReturn = bool;
+  };
 
   // Add a separate method to get the value of each field, called getDestination,
+  public String getDestination(){
+    return destination;
+  };
+
   // getPrice and getIsReturn.
+  public double getPrice(){
+    return price;
+  };
+  public boolean getIsReturn(){
+    return isReturn;
+  };
 
 }
--- _02_05b/Ticket.java
+++ _02_05b/Ticket.java
@@ -12,8 +12,25 @@ public class Ticket {
 
   // Add three public methods to set the value of each field, called
   // setDestination, setPrice and setIsReturn.
+  public void setDestination(String dest) {
+    this.destination = dest;
+  };
+  public void setPrice(double cost){
+    this.price = cost;
+  };
+  public void setIsReturn(boolean bool){
+    this.isReturn = bool;
+  };
 
   // Add three public methods to get the value of each field, called
   // getDestination, getPrice and getIsReturn.
-
+  public String getDestination(){
+    return destination;
+  };
+  public double getPrice(){
+    return price;
+  };
+  public boolean getIsReturn(){
+    return isReturn;
+  };
 }
